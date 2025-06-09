338701f9f46f8663102f002ab350b8e721ccb53c
Siva Sridhar Natuva <sridhar.natuva@outlook.com>
2025-06-09 15:55:39 +0530

    refactor: remove StudentsComponent and related files from the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { StudentsComponent } from './components/students/students.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'students', component: StudentsComponent }
+    { path: 'dashboard', component: DashboardComponent }
 
 ];
